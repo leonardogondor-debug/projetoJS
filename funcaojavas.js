9f67b6478943a9f321dfb675cf4da8c0c550c03e
leovit <leonardogondor@gmail.com>
2025-09-12 06:13:29 -0300

    corrige a função de cálculo do IMC e ajusta a chamada no botão

--- funcaojavas.js
+++ funcaojavas.js
@@ -1,6 +1,9 @@
-function peso-altura() {
+function pesoaltura() {
   // corpo da função
   let valorpeso = document.getElementById("peso").value;
   let valoraltura = document.getElementById("altura").value;
   
+  let imc = valorpeso / (valoraltura + valoraltura);
+  
+  document.getElementById("resultado").textContent = "seu imc e " + imc;
 } 
