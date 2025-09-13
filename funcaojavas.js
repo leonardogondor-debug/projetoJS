296c53850b9a1789b3cdd3c976bf01166cbf9748
leovit <leonardogondor@gmail.com>
2025-09-12 23:52:35 -0300

    ajusta a exibição do IMC e corrige a lógica de resultados no cálculo

--- funcaojavas.js
+++ funcaojavas.js
@@ -6,11 +6,13 @@ function pesoaltura() {
   let imc = valorpeso / (valoraltura * valoraltura);
   let imcFormatado = imc.toFixed(2);
   
+  document.getElementById("resultado").textContent = "seu imc é " + imcFormatado;
+   
 
   if (imc < 18.5)
-    document.getElementById("resultado").textContent = "seu imc é " + imcFormatado + ", você está abaixo do peso";
+    document.getElementById("resultado2").textContent = "você está abaixo do peso";
   else if (imc < 24.9)
-    document.getElementById("resultado").textContent = "seu imc é " + imcFormatado + ", você está com o peso ideal";
+    document.getElementById("resultado2").textContent = "você está com o peso ideal";
   else if (imc < 29.9)
-    document.getElementById("resultado").textContent = "seu imc é " + imcFormatado + ", você está com sobrepeso";
+    document.getElementById("resultado2").textContent = "você está com sobrepeso";
 }
