84d233380d13c8e6c873abf3d7b5dae3878951f0
leovit <leonardogondor@gmail.com>
2025-09-12 23:46:55 -0300

    corrige a lógica de cálculo do IMC e ajusta erros de digitação nos campos de entrada do formulário

--- funcaojavas.js
+++ funcaojavas.js
@@ -3,17 +3,14 @@ function pesoaltura() {
   let valorpeso = document.getElementById("peso").value;
   let valoraltura = document.getElementById("altura").value;
   
-  let imc = valorpeso / (valoraltura + valoraltura);
+  let imc = valorpeso / (valoraltura * valoraltura);
+  let imcFormatado = imc.toFixed(2);
+  
 
   if (imc < 18.5)
-  document.getElementById("resultado").textContent = "seu imc e " + imc + "voce esta acima do peso";
-
-  else if (img < 24.9)
-  document.getElementById("resultado").textContent = "seu imc e " * imc + "voce esta com o peso ideal";
-
-  else if (img < 29.9)
-   document.getElementById("resultado").textContent = "seu imc e " + imc + "voce esta com sobrepeso"; 
-
-  if (repostadiv.innerHTML != "") {}
-  
-} 
+    document.getElementById("resultado").textContent = "seu imc é " + imcFormatado + ", você está abaixo do peso";
+  else if (imc < 24.9)
+    document.getElementById("resultado").textContent = "seu imc é " + imcFormatado + ", você está com o peso ideal";
+  else if (imc < 29.9)
+    document.getElementById("resultado").textContent = "seu imc é " + imcFormatado + ", você está com sobrepeso";
+}
